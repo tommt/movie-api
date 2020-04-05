63bbc1c748ea899e6d642d5e7b06728928c7fbe6
suraj.iwacademy <suraj.iwacademy@gmail.com>
2020-04-05 08:33:32 +0545

    Created models

--- src/api/models.py
+++ src/api/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.conf import settings
+from django.core.validators import MaxValueValidator, MinValueValidator
 
-# Create your models here.
+
+class Movie(models.Model):
+    title = models.CharField(max_length=50)
+    description = models.TextField(max_length=360)
+
+    def __str__(self):
+        return self.title
+
+
+
+class Rating(models.Model):
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    stars = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
+
+    class Meta:
+        unique_together = (('user', 'movie'),)
+        index_together = (('user', 'movie'),)
+
+    
+    def __str__(self):
+        return self.user.username
